3415c3711353e4ea922e40f38d2f8e680e03ecb9
dcs <dcstrandberg@gmail.com>
2016-04-30 12:30:14 -0500

    The first step in making Candor. I made it so that chat messages appear when you're typing. I'm still only at the point that the messages appear after you press submit, but it's something.
    Things to look into:
    -I had to change the textarea input into a regular text input because the :focus CSS selector wouldn't change it's background color. It would add a border and everything else, but not change its background color.
    -I think the submit button isn't responding to the CSS changes I make for increasing the transition time, and possibly others.
    -The messages aren't ordered in reverse order. I've got to figure out the orderBy filter or potentially the CSS flex column property.
    -I can't get the submitted words to be deleted from the input area.

---
+++ candor.js
@@ -0,0 +1,38 @@
+var app = angular.module("discApp", []);
+app.controller('discCtrl', function($scope, $http, $timeout){
+    //Get the necessary info from the "database"
+    /*$http.get("candorTest.json")
+        .then(function(response) {
+           //Assign the user object to name
+           $scope.name = response.data;
+    });*/
+    $scope.name = {
+        "name": "David",
+        "msgList": []
+    };
+    $scope.sendMsg = function() {
+        var newMsg = {
+            "text": $scope.typedMsg,
+            "datetime": new Date(),
+            "msgClass": "sent"    
+        };
+        //After creating the new msg object, push it to the msgList array
+        $scope.name.msgList.push( newMsg );
+        //Clear the input field
+        $scope.typedMsg = "";
+        //Create a sample received message to test the CSS
+        $timeout( function() {
+            var receivedMsg = {
+            "text": "Message Received",
+            "datetime": new Date(),
+            "msgClass": "received"    
+            };
+            $scope.name.msgList.push( receivedMsg ); 
+        }, 2000);
+    };
+});//End of discCtrl
+
+app.service('', function(typedMsg) {
+    //Eventually have something to write the current text to the database
+    
+});
